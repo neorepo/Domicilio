d8be33c3f9881c0afc0fb5779942af12e822bc18
neo <neorepo@gmail.com>
2020-09-23 08:45:11 -0300

    Se realizan pruebas

--- src/domicilio/Main.java
+++ src/domicilio/Main.java
@@ -15,13 +15,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Asociado asociado1 = new Asociado("Homero", "Simpson", "M", "03/08/1981");
-        Asociado asociado2 = new Asociado("Marge", "Simpson", "F", "08/03/1986");
+
+        Persona asociado = new Asociado("Lisa", "Simpson", "F", "23/01/2005");
 
         Domicilio domicilio1 = new Domicilio("Falsa", "123", "2", "3", "Springfield", "321", "United States", "US-S");
-
-        asociado1.setEstaDomiciliadoEn(domicilio1);
+        Asociado asociado1 = new Asociado("Homero", "Simpson", "M", "03/08/1981", domicilio1);
+        
+        Asociado asociado2 = new Asociado("Marge", "Simpson", "F", "08/03/1986");
         asociado2.setEstaDomiciliadoEn(domicilio1);
 
         System.out.println(asociado1);
